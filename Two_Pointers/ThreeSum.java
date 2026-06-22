5259ceebb10c36ba3697345adcb531e1fd088fe1
Adway Gaikwad <adwaykumar44@gmail.com>
2026-06-22 23:38:52 +0530

    three sum added

---
+++ Two_Pointers/ThreeSum.java
@@ -0,0 +1,54 @@
+// 15. 3Sum
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums={-1,0,1,2,-1,-4};
+        
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int f = 0; f < nums.length; f++) {
+            if (nums[f]>0) {
+                break;
+            }
+            if (nums[f]==0 && nums[f]!=nums[f-1]) {
+                helper_two_sum(f, nums, res);
+            }
+        }
+
+        return res;
+    }
+    public void helper_two_sum(int f,int [] nums,List<List<Integer>> res){
+        int i = f+1;
+        int j = nums.length-1;
+
+        while (i<j) {
+            int sum = nums[f]+nums[i]+nums[j];
+            if (sum>0) {
+                j--;
+            }
+            else if (sum<0) {
+                i++;
+            }
+            else{
+                res.add(Arrays.asList(nums[f],nums[i],nums[j]));
+                i++;
+                j--;
+            }
+
+            while (i<j && nums[i] == nums[i-1]) {
+                i++;
+            }
+
+            while (i<j && nums[j] == nums[j+1]) {
+                j--;
+            }
+        }
+    }
+}
